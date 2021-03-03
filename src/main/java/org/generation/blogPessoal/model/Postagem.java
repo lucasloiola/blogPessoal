ba1f67cf26add1a5647c11e8c4b46cfe07625306
Lucas <lucasloiolaalmeida@gmail.com>
2021-03-02 21:21:16 -0300

    Incluindo controller tema

--- src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,11 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity
@@ -28,11 +30,14 @@ public class Postagem {
 	@NotNull
 	@Size(min = 10, max = 500)
 	private String texto;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
 
-	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
 	public long getId() {
 		return id;
 	}
@@ -64,6 +69,13 @@ public class Postagem {
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	
-	
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
 }
