b33f6eba73f241284f3137437cd071c5655cab14
Lucas <lucasloiolaalmeida@gmail.com>
2021-03-02 22:02:39 -0300

    Incluindo classe de configuração de segurança

--- src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -6,7 +6,5 @@ import org.generation.blogPessoal.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-	
 	public Optional<Usuario> findByUsuario(String usuario);
-
 }
