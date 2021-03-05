08ddb2fcb767fc3bdb929d066e62d623d590dd33
Lucas <lucasloiolaalmeida@gmail.com>
2021-03-05 20:06:09 -0300

    refactor(UsuarioController): Teste unitário

--- src/main/java/org/generation/blogPessoal/Controller/UsuarioController.java
+++ src/main/java/org/generation/blogPessoal/Controller/UsuarioController.java
@@ -1,14 +1,17 @@
 package org.generation.blogPessoal.Controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.UsuarioRepository;
 import org.generation.blogPessoal.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,17 +23,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
 	@Autowired
+	private UsuarioRepository repository;
+
+	@Autowired
 	private UsuarioService usuarioService;
 	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+
 	@PostMapping("/logar")
-	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user) {
 		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
-	
+
 	@PostMapping("/cadastrar")
-	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(usuarioService.CadastrarUsuario(usuario));
+	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
+		Optional<Usuario> user = Optional.ofNullable(usuarioService.CadastrarUsuario(usuario));
+		try {
+			return ResponseEntity.ok(user.get());
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+
 	}
 }
